1cc259e87bf36aca16172eafbc2a3184f776295a
AlexisLuna-coder <alexissanchezluna5@gmail.com>
2026-03-25 14:22:40 -0600

    guardando cambios antes de pull

--- resources/views/libros/home.blade.php
+++ resources/views/libros/home.blade.php
@@ -11,23 +11,9 @@
 <body>
     <h1>LIBROS DISPONIBLES</h1>
     
-    <div style="display: flex; flex-wrap: wrap; gap: 20px;">
-        @foreach($history as $libro)
-            <div style="width: 200px;">
-                <!-- Título del libro -->
-                <h3>
-                    {{$libro['volumeInfo']['title'] ?? 'Sin título' }}
-                </h3>
-                <!-- Autor del libro -->
-                <p>
-                    {{ $libro['volumeInfo']['authors'][0] ?? 'Autor desconocido' }}
-                </p>
-                <!-- Imagen del libro -->
-                @if(isset($libro['volumeInfo']['imageLinks']['thumbnail']))
-                    <img src="{{ $libro['volumeInfo']['imageLinks']['thumbnail'] }}" alt="">
-                @endif
-            </div>
-        @endforeach
+    <div class = "main-container">
+        <!-- SIDEBAR
+        CONTENIDO -->
     </div>
 </body>
 </html>
